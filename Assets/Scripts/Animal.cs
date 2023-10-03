aba0e82db44ceb1eb7847b13105c2479ba5a353d
dazul99 <david019lb@gmail.com>
2023-10-03 12:56:54 +0200

    uwu

---
+++ Assets/Scripts/Animal.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+    [SerializeField] private int numberOfPaws = 4;
+}
---
+++ Assets/Scripts/FirstScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class FirstScript : MonoBehaviour
+{
+    //Pepe el bardo   
+    //Edad del bardo
+    private int age = 24;
+    //Genero del bardo
+    private bool male = true;
+    //Nombre del bardo
+    private string name = "Pepe";
+    // B A R D O
+    private string profession = "Bardo";
+    //Justo lo que parece
+    private float price = 53.88f;
+    /*
+     * Pepe
+     * El
+     * Bardo
+     */
+}
