53fdc1caa70f9223d2017bce99839e98482b8744
dazul99 <david019lb@gmail.com>
2023-10-10 12:47:45 +0200

    sa

--- Assets/Scripts/Operations.cs
+++ Assets/Scripts/Operations.cs
@@ -6,13 +6,17 @@ public class Operations : MonoBehaviour
 {
     [SerializeField] private float num1;
     [SerializeField] private float num2;
+    [SerializeField] private int num;
 
     private void Start()
     {
-        Debug.Log(num1 + "+" + num2 + "=" + (num1 + num2));
-        Debug.Log(num1 + "-" + num2 + "=" + (num1 - num2));
-        Debug.Log(num1 + "*" + num2 + "=" + (num1 * num2));
-        Debug.Log(num1 + "/" + num2 + "=" + (num1 / num2));
-        Debug.Log(num1 + "%" + num2 + "=" + (num1 % num2));
+        /*
+        Debug.Log(num1 + " + " + num2 + " = " + (num1 + num2));
+        Debug.Log(num1 + " - " + num2 + " = " + (num1 - num2));
+        Debug.Log(num1 + " * " + num2 + " = " + (num1 * num2));
+        Debug.Log(num1 + " / " + num2 + " = " + (num1 / num2));
+        Debug.Log(num1 + " % " + num2 + " = " + (num1 % num2));
+        */
+        Debug.Log(num + " % 69 = " + (num % 69));
     }
 }
